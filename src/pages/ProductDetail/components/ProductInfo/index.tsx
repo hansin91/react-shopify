cdf6005a5be0ed6eed8c0d7d34173548ea7b107a
hansin91 <hansinsusatya@gmail.com>
2020-09-22 05:12:50 +0700

    Add to bag

--- src/pages/ProductDetail/components/ProductInfo/index.tsx
+++ src/pages/ProductDetail/components/ProductInfo/index.tsx
@@ -2,7 +2,7 @@ import './styles.scss'
 import React, { useCallback } from 'react'
 import parse from 'react-html-parser'
 import { debounce } from 'lodash'
-import { Form } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { Product } from '../../../../interfaces'
 
@@ -23,6 +23,10 @@ function ProductInfo({product, collection, onFocusInput, onImageFile, onNamePers
   const handleFile = (e: any) => {
     e.persist();
     onImageFile(e.target.files[0])
+  }
+
+  const handleQuantity = (quantity: number) => {
+    console.log(quantity)
   }
 
   return(
@@ -50,6 +54,17 @@ function ProductInfo({product, collection, onFocusInput, onImageFile, onNamePers
                   <Form.Group>
                     <Form.Label className="pplrlabel">Image</Form.Label>
                     <Form.Control onChange={handleFile}  type="file" accept="image/*"></Form.Control>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label className="pplrlabel">Quantity</Form.Label>
+                    <input type="number"
+                      onChange={(e: any) => handleQuantity(Number(e.target.value))}
+                      min="1"
+                      className="pplrlabel form-control"
+                      defaultValue="1" />
+                  </Form.Group>
+                  <Form.Group>
+                   <input type="submit" className="btn btn-primary p_a_t_c purchase-btn" value="Add to bag"/>
                   </Form.Group>
                 </Form>
               </div>
